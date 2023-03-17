b315c8cb05f7f3957533f085ca3be83270fbd57b
kushankur-wisdm <kushankur.das@wisdmlabs.com>
2023-03-17 19:07:27 +0530

    Formatting

--- functions.php
+++ functions.php
@@ -1,24 +1,27 @@
 <?php
 
-add_theme_support( 'custom-logo');
-add_theme_support( 'title-tag' );
+add_theme_support('custom-logo');
+add_theme_support('title-tag');
 
 
 // Load in our CSS
-function lawyer_enqueue_styles() {
+function lawyer_enqueue_styles()
+{
 
-    wp_enqueue_style( 'custom-css', get_template_directory_uri() . '/assets/css/custom.css' );
+	wp_enqueue_style('custom-css', get_template_directory_uri() . '/assets/css/custom.css');
 }
-add_action( 'wp_enqueue_scripts', 'lawyer_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'lawyer_enqueue_styles');
 
 //Nav Menu
-if ( ! function_exists( 'plugin_register_nav_menu' ) ) {
+if (!function_exists('plugin_register_nav_menu')) {
 
-	function plugin_register_nav_menu(){
-		register_nav_menus( array(
-	    	'primary_menu' => __( 'Primary Menu', 'lawyer' ),
-			'footer_menu' => __( 'Footer Menu', 'lawyer' ),
-		) );
+	function plugin_register_nav_menu()
+	{
+		register_nav_menus(
+			array(
+				'primary_menu' => __('Primary Menu', 'lawyer'),
+				'footer_menu' => __('Footer Menu', 'lawyer'),
+			));
 	}
-	add_action( 'after_setup_theme', 'plugin_register_nav_menu');
+	add_action('after_setup_theme', 'plugin_register_nav_menu');
 }
--- index.php
+++ index.php
@@ -1,154 +1,155 @@
 <?php
-    get_header();
+get_header();
 ?>
 
 <section class="banner">
-      <div class="banner__right">
-        <img
-          id="banner__image"
-          src="<?php echo (get_template_directory_uri(). '/assets/images/hero__image.png') ?>"
-          alt="Banner Image"
-        />
-        <div class="hero__desc center">
-          <p>Tiara Andini</p>
-          <p>-Lawyer</p>
-        </div>
-      </div>
-      <div class="banner__toast">
-        <span>New</span>
-        <span>Stay connected to the upcoming & Recent jobs</span>
-      </div>
-      <h1>
-        Your Solution <br />
-        Legal Consultacy
-      </h1>
+  <div class="banner__right">
+    <img id="banner__image" src="<?php echo (get_template_directory_uri() . '/assets/images/hero__image.png') ?>"
+      alt="Banner Image" />
+    <div class="hero__desc center">
+      <p>Tiara Andini</p>
+      <p>-Lawyer</p>
+    </div>
+  </div>
+  <div class="banner__toast">
+    <span>New</span>
+    <span>Stay connected to the upcoming & Recent jobs</span>
+  </div>
+  <h1>
+    Your Solution <br />
+    Legal Consultacy
+  </h1>
+  <p>
+    We are here to help you take care of your legality <br />
+    with the best service especially for you.
+  </p>
+  <button class="banner__button">GET STARTED</button>
+  <p>Trusted by 10+ companies in indonesia</p>
+  <img src="<?php echo (get_template_directory_uri() . '/assets/images/company__logo.png') ?>"
+    alt="Our Partner Brand Logo" />
+</section>
+<section class="help center">
+  <div class="help__content">
+    <h1>Why do we help with legalization?</h1>
+    <p>
+      We are here for UMKM in Indonesia to carry out the legalization
+      process, which is sometimes complicated.
+    </p>
+  </div>
+  <div class="help__boxes center">
+    <div>
+      <img src="<?php echo (get_template_directory_uri() . '/assets/images/circle__layer.png') ?>" alt="Circle Icon" />
+      <h3>Enviromental Law</h3>
       <p>
-        We are here to help you take care of your legality <br />
-        with the best service especially for you.
+        Environmental Legal Issue might occur since the planned business
+        activities are designed
       </p>
-      <button class="banner__button">GET STARTED</button>
-      <p>Trusted by 10+ companies in indonesia</p>
-      <img src="<?php echo (get_template_directory_uri(). '/assets/images/company__logo.png') ?>" alt="Our Partner Brand Logo" />
-    </section>
-    <section class="help center">
-      <div class="help__content">
-        <h1>Why do we help with legalization?</h1>
+    </div>
+    <div>
+      <img src="<?php echo (get_template_directory_uri() . '/assets/images/bag.png') ?>" alt="Bag Icon" />
+      <h3>Enviromental Law</h3>
+      <p>
+        Environmental Legal Issue might occur since the planned business
+        activities are designed
+      </p>
+    </div>
+    <div>
+      <img src="<?php echo (get_template_directory_uri() . '/assets/images/rocket.png') ?>" alt="Rocket Icon" />
+      <h3>Enviromental Law</h3>
+      <p>
+        Environmental Legal Issue might occur since the planned business
+        activities are designed
+      </p>
+    </div>
+    <div>
+      <img src="<?php echo (get_template_directory_uri() . '/assets/images/user__arrows.png') ?>" alt="Arrow Icon" />
+      <h3>Enviromental Law</h3>
+      <p>
+        Environmental Legal Issue might occur since the planned business
+        activities are designed
+      </p>
+    </div>
+  </div>
+</section>
+<section class="count center"
+  style="background: url(<?php echo (get_template_directory_uri() . '/assets/images/bg__pattern.png') ?>);">
+  <h1>Some count that matters</h1>
+  <p>Our achievement in the journey depicted in numbers</p>
+  <div class="count__boxes center">
+    <div class="center" id="count__leftBox">
+      <h1>30</h1>
+      <p>Clients</p>
+    </div>
+    <div class="center" id="count__centerBox">
+      <h1>300+</h1>
+      <p>Taken business legalities</p>
+    </div>
+    <div class="center">
+      <h1>8</h1>
+      <p>Years of Journey</p>
+    </div>
+  </div>
+</section>
+<section class="testimonial center">
+  <h1>Clients Testimonial</h1>
+  <div class="testimonial__content center">
+    <div class="testimonial__div center">
+      <div class="center">
+        <h3>Incredible Experience</h3>
         <p>
-          We are here for UMKM in Indonesia to carry out the legalization
-          process, which is sometimes complicated.
+          We had an incredible experience working with Landify and were
+          impressed they made such a big difference in only three weeks. Our
+          team is so grateful for the wonderful improvements they made and
+          their ability to get familiar with the concept so quickly.
         </p>
+        <div class="triangle"></div>
       </div>
-      <div class="help__boxes center">
+      <div class="testimonial__testimony center">
+        <img src="<?php echo (get_template_directory_uri() . '/assets/images/testimony__image.png') ?>"
+          alt="Testimonial Image" />
         <div>
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/circle__layer.png') ?>" alt="Circle Icon" />
-          <h3>Enviromental Law</h3>
-          <p>
-            Environmental Legal Issue might occur since the planned business
-            activities are designed
-          </p>
-        </div>
-        <div>
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/bag.png') ?>" alt="Bag Icon" />
-          <h3>Enviromental Law</h3>
-          <p>
-            Environmental Legal Issue might occur since the planned business
-            activities are designed
-          </p>
-        </div>
-        <div>
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/rocket.png') ?>" alt="Rocket Icon" />
-          <h3>Enviromental Law</h3>
-          <p>
-            Environmental Legal Issue might occur since the planned business
-            activities are designed
-          </p>
-        </div>
-        <div>
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/user__arrows.png') ?>" alt="Arrow Icon" />
-          <h3>Enviromental Law</h3>
-          <p>
-            Environmental Legal Issue might occur since the planned business
-            activities are designed
-          </p>
+          <h4>Anya Tailor Joy</h4>
+          <p>CEO, SF Industires</p>
         </div>
       </div>
-    </section>
-    <section class="count center" style="background: url(<?php echo (get_template_directory_uri(). '/assets/images/bg__pattern.png') ?>);">
-      <h1>Some count that matters</h1>
-      <p>Our achievement in the journey depicted in numbers</p>
-      <div class="count__boxes center">
-        <div class="center" id="count__leftBox">
-          <h1>30</h1>
-          <p>Clients</p>
-        </div>
-        <div class="center" id="count__centerBox">
-          <h1>300+</h1>
-          <p>Taken business legalities</p>
-        </div>
-        <div class="center">
-          <h1>8</h1>
-          <p>Years of Journey</p>
-        </div>
-      </div>
-    </section>
-    <section class="testimonial center">
-      <h1>Clients Testimonial</h1>
-      <div class="testimonial__content center">
-        <div class="testimonial__div center">
-          <div class="center">
-            <h3>Incredible Experience</h3>
-            <p>
-              We had an incredible experience working with Landify and were
-              impressed they made such a big difference in only three weeks. Our
-              team is so grateful for the wonderful improvements they made and
-              their ability to get familiar with the concept so quickly.
-            </p>
-            <div class="triangle"></div>
-          </div>
-          <div class="testimonial__testimony center">
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/testimony__image.png') ?>" alt="Testimonial Image" />
-            <div>
-              <h4>Anya Tailor Joy</h4>
-              <p>CEO, SF Industires</p>
-            </div>
-          </div>
-        </div>
-        <div class="testimonial__div center">
-          <div class="center">
-            <h3>Dependable, Responsive, Professional Partner</h3>
-            <p>
-              Fermin Apps has collaborated with Landify team for several
-              projects such as Photo Sharing Apps and Custom Social Networking
-              Apps. The experience has been pleasant, professional and exceeding
-              our expectations.
-            </p>
-            <div class="triangle"></div>
-          </div>
-          <div class="testimonial__testimony center">
-            <img src="<?php echo (get_template_directory_uri(). '/assets/images/testimony__image.png') ?>" alt="Testimonial Image" />
-            <div>
-              <h4>Sri Alam</h4>
-              <p>CEO, Membagongkan GROUP</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="question center">
-      <div>
-        <h1>Complex Questions?</h1>
+    </div>
+    <div class="testimonial__div center">
+      <div class="center">
+        <h3>Dependable, Responsive, Professional Partner</h3>
         <p>
-          Request for a personalized budget for your legal problem. We will send
-          you a coupe options in 24 hours. You can have free consult , if a our
-          first customer
+          Fermin Apps has collaborated with Landify team for several
+          projects such as Photo Sharing Apps and Custom Social Networking
+          Apps. The experience has been pleasant, professional and exceeding
+          our expectations.
         </p>
-        <div class="question__button center">
-          <img src="<?php echo (get_template_directory_uri(). '/assets/images/call__logo.png') ?>" alt="Call Logo" />
-          <span>Call now</span>
+        <div class="triangle"></div>
+      </div>
+      <div class="testimonial__testimony center">
+        <img src="<?php echo (get_template_directory_uri() . '/assets/images/testimony__image.png') ?>"
+          alt="Testimonial Image" />
+        <div>
+          <h4>Sri Alam</h4>
+          <p>CEO, Membagongkan GROUP</p>
         </div>
       </div>
-      <img src="<?php echo (get_template_directory_uri(). '/assets/images/man__image.png') ?>" alt="Man Image" />
-    </section>
+    </div>
+  </div>
+</section>
+<section class="question center">
+  <div>
+    <h1>Complex Questions?</h1>
+    <p>
+      Request for a personalized budget for your legal problem. We will send
+      you a coupe options in 24 hours. You can have free consult , if a our
+      first customer
+    </p>
+    <div class="question__button center">
+      <img src="<?php echo (get_template_directory_uri() . '/assets/images/call__logo.png') ?>" alt="Call Logo" />
+      <span>Call now</span>
+    </div>
+  </div>
+  <img src="<?php echo (get_template_directory_uri() . '/assets/images/man__image.png') ?>" alt="Man Image" />
+</section>
 
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
